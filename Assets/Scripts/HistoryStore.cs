5abbee5b044183ccdeaa97787cd694aa20356995
Girrint <truongphan172@gmail.com>
2026-05-06 04:53:08 +0700

    ui: add history persistence and leaderboard rendering flows

---
+++ Assets/Scripts/HistoryStore.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ScoreList
+{
+	public List<int> scores = new List<int>();
+}
+
+public static class HistoryStore
+{
+	private const string PrefsKey = "LeaderboardV1";
+	private const int MaxScores = 20;
+
+	public static void AddScore(int score)
+	{
+		ScoreList list = LoadList();
+
+		list.scores.Add(score);
+
+		list.scores.Sort((a, b) => b.CompareTo(a));
+
+		if (list.scores.Count > MaxScores)
+		{
+			list.scores.RemoveRange(MaxScores, list.scores.Count - MaxScores);
+		}
+
+		SaveList(list);
+	}
+
+	public static List<int> GetScores()
+	{
+		return LoadList().scores;
+	}
+
+	public static void Clear()
+	{
+		PlayerPrefs.DeleteKey(PrefsKey);
+		PlayerPrefs.Save();
+	}
+
+	private static ScoreList LoadList()
+	{
+		string json = PlayerPrefs.GetString(PrefsKey, "");
+
+		if (string.IsNullOrEmpty(json))
+			return new ScoreList();
+
+		try
+		{
+			return JsonUtility.FromJson<ScoreList>(json) ?? new ScoreList();
+		}
+		catch
+		{
+			return new ScoreList();
+		}
+	}
+
+	private static void SaveList(ScoreList list)
+	{
+		string json = JsonUtility.ToJson(list);
+
+		PlayerPrefs.SetString(PrefsKey, json);
+		PlayerPrefs.Save();
+	}
+}
---
+++ Assets/Scripts/LeaderboardUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LeaderboardUI : MonoBehaviour
+{
+	public TMP_Text historyText;
+
+	private void OnEnable()
+	{
+		RefreshLeaderboard();
+	}
+
+	public void RefreshLeaderboard()
+	{
+		List<int> scores = HistoryStore.GetScores();
+
+		historyText.text = "";
+
+		for (int i = 0; i < scores.Count; i++)
+		{
+			string rank = "";
+
+			if (i == 0)
+			{
+				rank = "<size=250%><voffset=0.1em><sprite=0></voffset></size>";
+				historyText.text += $"<indent=5px>{rank}<space=2px>SCORE : {scores[i]}\n\n";
+			}
+			else if (i == 1)
+			{
+				rank = "<size=250%><voffset=0.1em><sprite=1></voffset></size>";
+				historyText.text += $"<indent=5px>{rank}<space=2px>SCORE : {scores[i]}\n\n";
+			}
+			else if (i == 2)
+			{
+				rank = "<size=250%><voffset=0.1em><sprite=2></voffset></size>";
+				historyText.text += $"<indent=5px>{rank}<space=2px>SCORE : {scores[i]}\n\n";
+			}
+			else
+			{
+				rank = (i + 1).ToString();
+
+				historyText.text += $"<indent=22px>{rank}   SCORE : {scores[i]}\n";
+			}
+		}
+	}
+}
